39a20be0d521ac49244581234ce21842b5aca817
Estéban DESESSARD <edesessard@iutbayonne.univ-pau.fr>
2025-09-25 10:02:13 +0200

    Création de la méthode `add()` et `devide()`

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,5 +1,11 @@
 package calculator;
 
 public class Calculator {
+    public int add(int opG, int opD) {
+        return opG + opD;
+    }
 
+    public int devide(int opG, int opD) {
+        return opG / opD;
+    }
 }
