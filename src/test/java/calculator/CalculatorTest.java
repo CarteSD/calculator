e6cbd024d5bbc3cd72d2cc75125ffcb9a7fe5b23
Estéban DESESSARD <edesessard@iutbayonne.univ-pau.fr>
2025-09-25 10:16:17 +0200

    Écriture des deux premiers tests

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,7 +1,36 @@
 package calculator;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 class CalculatorTest {
+    @Test
+    void add_devrait_retourner_la_somme_de_deux_entiers() {
+        // Given
+        Calculator calculator = new Calculator();
+        int opG = 5;
+        int opD = 3;
 
+        // When
+        int result = calculator.add(opG, opD);
+
+        // Then
+        assertThat(result).isEqualTo(8);
+    }
+
+    @Test
+    void devide_devrait_retourner_le_quotient_de_deux_entiers() {
+        // Given
+        Calculator calculator = new Calculator();
+        int opG = 10;
+        int opD = 2;
+
+        // When
+        int result = calculator.devide(opG, opD);
+
+        // Then
+        assertThat(result).isEqualTo(5);
+    }
 }
